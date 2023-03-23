8db97ed33c648e63e4be752dbbfe1f49f502b23b
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-23 20:57:26 -0300

    HTML dos itens da lista

--- todo-list/src/components/TaskList.tsx
+++ todo-list/src/components/TaskList.tsx
@@ -14,7 +14,14 @@ const TaskList = ({ taskList }: Props) => {
       {taskList.length > 0 ? (
         taskList.map((task) => (
           <div key={task.id}>
-            {task.title}
+            <div>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div>
+              <i className='bi bi-pencil'></i>
+              <i className='bi bi-trash'></i>
+            </div>
           </div>
         ))
       ) : (
