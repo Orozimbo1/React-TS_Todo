b22c909fe1b8525c53188dd46c7e08880249fb16
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-23 19:26:24 -0300

    Criando Header e Footer

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
 
+// Components
+import Footer from './components/Footer'
+import Header from './components/Header'
+
 function App() {
 
   return (
     <div className="App">
-      <h1>Todo React</h1>
+      <Header />
+      <h1>Conteúdo</h1>
+      <Footer />
     </div>
   )
 }
