7d2fa2e532abe0cc51f11dab9a850c8de7ebf3bb
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-03-23 19:48:27 -0300

    Criando formulário

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -3,6 +3,8 @@ import styles from './App.module.css'
 // Components
 import Footer from './components/Footer'
 import Header from './components/Header'
+import TaskList from './components/TaskList'
+import Taskform from './components/Taskform'
 
 function App() {
 
@@ -10,7 +12,14 @@ function App() {
     <div className="App">
       <Header />
       <main className={styles.main}>
-        <h2>Conteudo</h2>
+        <div>
+          <h2>O que você vai fazer?</h2>
+          <Taskform btnText='Criar tarefa' />
+        </div>
+        <div>
+          <h2>Suas tarefas:</h2>
+          <TaskList />
+        </div>
       </main>
       <Footer />
     </div>
